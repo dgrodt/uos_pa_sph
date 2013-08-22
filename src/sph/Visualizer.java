801e4d61a5e7660cae70c0e56a3100d9444c07d7
sasbachm <sasbachm@cip35.informatik.uni-osnabrueck.de>
2013-08-22 11:30:10 +0200

    new Branch

--- src/sph/Visualizer.java
+++ src/sph/Visualizer.java
@@ -265,7 +265,7 @@ public class Visualizer extends FrameWork
         m_buffer[0].draw();
         
         //Swap back to Backbuffer and Draw Texture
-        frameBuffer.renderToBackbuffer(0);
+        frameBuffer.renderToBackbuffer(1);
 
         Display.update();
         
