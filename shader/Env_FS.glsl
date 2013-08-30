0545b8177f943c9cf23515f2261f4c7583abdcad
dgrodt <dgrodt@uos.de>
2013-08-30 10:11:45 +0200

    added distance fog

--- shader/Env_FS.glsl
+++ shader/Env_FS.glsl
@@ -10,14 +10,30 @@ uniform sampler2D ceilingTex;
 uniform sampler2D floorTex;
 uniform sampler2D wallTex;
 
+const vec4 fog_colour = vec4 (0.0, 0.0, 0.0, 1);
+const float min_fog_radius = 3.0;
+const float max_fog_radius = 10.0;
+
 out vec4 fs_out_color;
 void main()
 {
+	vec4 col;
+	
+	// work out distance from camera to point
+	float dist = length (g_eye-fs_in_ViewPos);
+	// get a fog factor (thickness of fog) based on the distance
+	float fog_fac = (dist - min_fog_radius) / (max_fog_radius - min_fog_radius);
+	// constrain the fog factor between 0 and 1
+	fog_fac = clamp (fog_fac, 0.0, 1.0);
+	
 	if(fs_in_normal.x == 0 && fs_in_normal.z == 0 && fs_in_normal.y > 0) {
-    	fs_out_color = texture(ceilingTex, fs_in_tc);
+    	col = texture(ceilingTex, fs_in_tc);
     } else if(fs_in_normal.x == 0 && fs_in_normal.z == 0 && fs_in_normal.y < 0) {
-    	fs_out_color = texture(floorTex, fs_in_tc);
+    	col = texture(floorTex, fs_in_tc);
     } else {
-    	fs_out_color = texture(wallTex, fs_in_tc);
+    	col = texture(wallTex, fs_in_tc);
     }
+    // blend the fog colour with the lighting colour, based on the fog factor
+	fs_out_color = mix (col, fog_colour, fog_fac);
+	fs_out_color.a = 1.0f;
 }
